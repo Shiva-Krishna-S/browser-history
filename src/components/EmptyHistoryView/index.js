03cb0f74975de862ca1e3179ea94840ddf41d396
ShivaKrishna <shiva.ramoju@gmail.com>
2023-12-02 23:08:12 +0000

    updated css

---
+++ src/components/EmptyHistoryView/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const EmptyHistoryView = () => (
+  <div className="empty-history-container">
+    <p className="empty-view-text">There is no history to show</p>
+  </div>
+)
+
+export default EmptyHistoryView
